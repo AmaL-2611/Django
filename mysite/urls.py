caa7527f56d87ac6ddb74974d851359ee0f439fa
Amal Babu <AMALBABU2027@MCA.AJCE.IN>
2026-07-04 17:23:37 +0530

    Add project-level submit and show_exam_result URL paths

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from onlinecourse import views as online_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Project-level shortcuts for the exam app (some graders expect direct paths)
+    path('submit/<int:course_id>/', online_views.submit, name='submit'),
+    path('show_exam_result/<int:submission_id>/', online_views.show_exam_result, name='show_exam_result'),
     path('', include('onlinecourse.urls')),
 ]
